313dc6bdcdf2306bd90ecb3c993afbdee199eec5
Alexey Buravov <night.lord.189@gmail.com>
2021-06-18 04:39:54 +0600

    initial

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	fmt.Println("start")
+
+	cfg, err := readConfig("config.json")
+	if err != nil {
+		log.Fatalf("error read config: %v", err)
+	}
+
+	r := gin.Default()
+	r.GET("/")
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": cfg["name"],
+			"time":    time.Now(),
+		})
+	})
+	r.Run(":8080")
+}
+
+func readConfig(path string) (map[string]interface{}, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		panic("LoadConfig error: " + err.Error())
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	var result map[string]interface{}
+	err = decoder.Decode(&result)
+	return result, err
+}
